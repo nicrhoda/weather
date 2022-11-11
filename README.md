1e8242580e9f35a4c622c41f278e8f5f33b640c9
nic <nrhoda14@gmail.com>
2022-11-11 17:32:16 -0600

    updated README and added screenshot

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # weather
+
+This web application is designed to display the weather in real time as well as display the forecast for the upcoming week.  Will soon be able to store searches to local storage and allow users to quickly find recent searches much faster.
+
+#URL
+
+https://nicrhoda.github.io/weather/
--- assets/js/script.js
+++ assets/js/script.js
@@ -25,26 +25,35 @@ function fetchRequest () {
     })
     .then(function(data){
       console.log(data);
+      //appends data from fetch request to the current weather display
       $('#templarge').append("Temp: " + data.list[0].main.temp);
       $('#windlarge').append("Wind speed: " + data.list[0].wind.speed);
       $('#humlarge').append("Humidity: " + data.list[0].main.humidity + "%");
+      //appends the day to the forecast elements
       $('#day1').append( moment().calendar());
       $('#day2').append( moment().add(1, 'days').calendar());
       $('#day3').append( moment().add(2, 'days').calendar());
       $('#day4').append( moment().add(3, 'days').calendar());
       $('#day5').append( moment().add(4, 'days').calendar());
-
+      //appends the temperature to the temp rows
       $('#temprow1').append("Temp: " + data.list[0].main.temp);
       $('#temprow2').append("Temp: " + data.list[1].main.temp);
       $('#temprow3').append("Temp: " + data.list[2].main.temp);
       $('#temprow4').append("Temp: " + data.list[3].main.temp);
       $('#temprow5').append("Temp: " + data.list[4].main.temp);
-      
+      //appends the wind speed to the wind rows
       $('#windrow1').append("Wind speed: " + data.list[0].wind.speed);
       $('#windrow2').append("Wind speed: " + data.list[0].wind.speed);
       $('#windrow3').append("Wind speed: " + data.list[0].wind.speed);
       $('#windrow4').append("Wind speed: " + data.list[0].wind.speed);
       $('#windrow5').append("Wind speed: " + data.list[0].wind.speed);
+      //appends the humidity to thr humidity rows
+      $('#humrow1').append("Humidity: " + data.list[0].main.humidity + "%");
+      $('#humrow2').append("Humidity: " + data.list[1].main.humidity + "%");
+      $('#humrow3').append("Humidity: " + data.list[2].main.humidity + "%");
+      $('#humrow4').append("Humidity: " + data.list[3].main.humidity + "%");
+      $('#humrow5').append("Humidity: " + data.list[4].main.humidity + "%");
+
 
       //reloads the page in order to clear old city and data
     $('#btn').on('click',function() {
